70826097256b2db063eb7ed147a6d20d8a9c4e1b
anji.rangappagari1 <anji.rangappagari1@gmail.com>
2026-02-25 22:28:33 +0800

    sample python program

---
+++ sample-demo/sample-demo.py
@@ -0,0 +1,8 @@
+# Your first mini-program
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print(f"Welcome {name}! You are an adult.")
+else:
+    print(f"Hi {name}, you're young and learning Python – great job!")
